18dd8b5188f321aa5d2adbec42a65e3888ea85d3
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-22 14:48:15 +0300

    last commit.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello world');
+    let amount = parseInt(count);
+    if(count <= 0) {
+      amount = 1
+    }
+    if(count > 8) {
+      amount = 8
+    }
+    setText(data.slice(0,amount))
   }  
   return (
   <section className="section-center">
@@ -21,8 +28,9 @@ function App() {
       <button type='submit' className='btn'>generate</button>
     </form>
     <article className='lorem-text'>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      {text.map((item,index) =>{
+        return <p key={index}>{item}</p>
+      })}
     </article>
   </section>
   );
